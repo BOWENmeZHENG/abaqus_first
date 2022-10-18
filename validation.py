eb8f266fe0f5a8270fa7f15634c887bbbb393d6a
BOWENmeZHENG <bowen_zheng@berkeley.edu>
2022-10-18 15:41:43 -0700

    small fix

--- validation.py
+++ validation.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
 data = pd.read_csv("nodes.csv")
-x, y, z = data.x[data.nodetype==1], data.y[data.nodetype==1], data.z[data.nodetype==1]
-xi, yi, zi = data.x[data.nodetype==0], data.y[data.nodetype==0], data.z[data.nodetype==0]
+x, y, z = data.x[data.nodetype == 1], data.y[data.nodetype == 1], data.z[data.nodetype == 1]
+xi, yi, zi = data.x[data.nodetype == 0], data.y[data.nodetype == 0], data.z[data.nodetype == 0]
 
 fig = plt.figure(figsize=(8, 6))
 ax = fig.add_subplot(projection='3d')
